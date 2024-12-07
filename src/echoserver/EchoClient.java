0df09bb0bae186c22c050593605f3a04343e4e9b
Simon H-M <simonhm31@gmail.com>
2024-12-07 17:32:54 -0600

    created threads

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,36 @@
 package echoserver;
 
 public class EchoClient {
+    public static final int PORT_NUMBER = 6013;
+
+        public static void main(String[] args) throws IOException {
+        EchoClient client = new EchoClient();
+        client.start();
+        }
+
+        private void start() throws IOException {
+            Socket socket = new Socket("localhost", PORT_NUMBER);
+            InputStream socketInputStream = socket.getInputStream();
+            OutputStream socketOutputStream = socket.getOutputStream();
+
+
+            Thread inputThread = new Thread(() -> {
+
+            });
+
+            Thread outputThread = new Thread(() -> {
+
+            });
+
+
+            inputThread.start();
+            outputThread.start();
+        }
+
+
+
+
+
+
+
 }
