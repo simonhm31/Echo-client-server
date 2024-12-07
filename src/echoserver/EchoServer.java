a42239887ca92de08c254eb3226ed21f91111b44
Simon H-M <simonhm31@gmail.com>
2024-12-07 16:53:22 -0600

    added server

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,9 @@
 package echoserver;
 
 public class EchoServer {
+    public static final int PORT_NUMBER = 6013;
+    public static void main(String[] args) throws IOException, InterruptedException {
+        EchoServer server = new EchoServer();
+        server.start();
+    }
 }
